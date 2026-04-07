44555cc808b1733957e71ca7e73b2bcc5a88704e
joshua-larks <jlarks@logitech.com>
2026-04-07 18:33:58 -0500

    feat: clean up config and prep for vite and vue migration.
    - Removed unused config files
    - Migrated from yarn to pnpm
    - Migrated from Azure to GitHub Actions

--- vite-publish.config.js
+++ vite-publish.config.js
@@ -4,22 +4,25 @@ import { createVuePlugin } from "vite-plugin-vue2";
 
 export default defineConfig({
   plugins: [createVuePlugin()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     minify: false,
-    target: "chrome61",
-    commonjsOptions: {
-      requireReturnsDefault: true,
-    },
+    target: "es2020",
+    cssCodeSplit: false,
     lib: {
       entry: resolve(__dirname, "src/system.js"),
       name: "Beaker",
       fileName: (format) => `beaker.${format}.js`,
     },
-    rollupOptions: {
-      external: ["vue"],
+    rolldownOptions: {
+      external: ["vue", "vue-router", "lodash-es"],
       output: {
         globals: {
           vue: "Vue",
+          "vue-router": "VueRouter",
+          "lodash-es": "_",
         },
       },
     },
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import { createVuePlugin } from "vite-plugin-vue2";
 export default defineConfig({
   base: "/beaker/",
   plugins: [createVuePlugin()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     minify: false,
     sourcemap: true,
-    commonjsOptions: {
-      requireReturnsDefault: true,
-    },
   },
 });
