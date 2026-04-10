58385bfb0a5a8fbe0e97fbf8862ac40b6281ca45
joshua-larks <jlarks@logitech.com>
2026-04-10 15:00:26 -0500

    backlog(task-17.1): Convert Home.vue to script setup - completed

--- backlog/tasks/task-17.1 - Convert-Home.vue-to-script-setup.md
+++ backlog/tasks/task-17.1 - Convert-Home.vue-to-script-setup.md
@@ -1,11 +1,11 @@
 ---
 id: TASK-17.1
 title: Convert Home.vue to script setup
-status: In Progress
+status: Done
 assignee:
   - Claude Code
 created_date: '2026-04-10 19:58'
-updated_date: '2026-04-10 19:58'
+updated_date: '2026-04-10 20:00'
 labels: []
 dependencies: []
 parent_task_id: TASK-17
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,16 +7,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "../components/HelloWorld.vue"; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+<script setup lang="ts">
+import HelloWorld from "../components/HelloWorld.vue";
 </script>
 
 <style lang="less" scoped>
